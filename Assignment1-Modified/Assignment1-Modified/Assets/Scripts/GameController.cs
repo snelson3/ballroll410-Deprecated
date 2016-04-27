f26c6f83476ad9186840b9305b8309fbaaad3d2c
bryonf <fleming_bryon@hotmail.com>
2016-04-27 11:10:26 -0700

    added unzipped modified assignment1 file as requested

---
+++ Assignment1-Modified/Assignment1-Modified/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public GameObject player;
+    public GameObject cam;
+    public Text countText;
+    public Text winText;
+
+    private int count;
+
+    // Use this for initialization
+    void Start ()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        cam.GetComponent<CameraController>().SetPlayer((GameObject)Instantiate(player, player.transform.position, player.transform.rotation));
+	}
+	
+	[SerializeField]
+    public void AddToCount()
+    {
+        count++;
+        SetCountText();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+}
