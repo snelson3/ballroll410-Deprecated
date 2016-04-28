a4002afe5b753222de8bd00abe9d006a9182ff42
Brian Davis <bwdavis11@gmail.com>
2016-04-28 16:42:24 -0700

    hamburgers are now the sole pickup

--- PoC_Build/Assets/Scripts/GameController.cs
+++ PoC_Build/Assets/Scripts/GameController.cs
@@ -78,6 +78,10 @@ public class GameController : MonoBehaviour {
         {
             StartCoroutine(LevelComplete());
         }
+       /* else
+        {
+            gameOverText.text = string.Format("Need {0} Morsels to advance", minMorsels-totalMorsels);
+        }*/
     }
 
     IEnumerator StartLevelMessage()
