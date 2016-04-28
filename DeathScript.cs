0bcbd6c2760935bef3d5cfb02fc31562229656cd
Adam Carlton <acarlton@uoregon.edu>
2016-04-27 18:04:28 -0700

    deathscript to attach to deathbox

---
+++ DeathScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathScript : MonoBehaviour {
+
+	void OnTriggerExit(Collider other) {
+		if(other.gameObject.CompareTag("player")) {
+			gameManager.GetComponent<GameController>().PlayerDestroy();
+		}
+	}
