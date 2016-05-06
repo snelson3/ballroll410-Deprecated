4f74750ddb079e423838872ec00ef456f8923c73
bryonf <fleming_bryon@hotmail.com>
2016-05-05 21:49:47 -0700

    PlayerController mechanics altered. Jump made to be more traditionally styled/more fluid.

--- PoC_Build/Assets/Scripts/PlayerController.cs
+++ PoC_Build/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
-	public float jump; //how long in seconds to jump
-	public float jumpScale; //How high to jump every second
+	public float jumpHeight;
 	public float upgradeAmt; //How much does the powerup increase speed
 	public float powerTime; //How long the powerup lasts in seconds
 
 	private Rigidbody rb;
     private GameController game;
-
-	private bool isJumping, isPowered;
-	private float upgrade;
-	private bool canJump;
-	private float jumpEnd;
-	private float powerEnd;
 	private AudioSource audioSource;
-
+	private bool isJumping, isPowered;
+	private float powerEnd;
+	private float upgrade;
 
 	void Start ()
 	{
@@ -29,48 +24,39 @@ public class PlayerController : MonoBehaviour {
 		audioSource = GetComponent<AudioSource> ();
 		upgrade = 1;
     }
-
+	
+	bool isGrounded() {
+		RaycastHit hit;
+		
+		//Casts a sphere of radius -0.1f less than the length of the radius of the player's sphere collider. This sphere is sent downward a maximum distance of 0.2f (0.1f below the
+		//collider boundary) and attempts to detect a collision with the ground.
+		return Physics.SphereCast(gameObject.transform.position, GetComponent<SphereCollider>().radius -0.1f, Vector3.down, out hit, 0.2f);
+	}
+	
 	void OnCollisionEnter(Collision collisionInfo) {
-		canJump = true;
+	    //This function is used to reduce occurrences of jumping before hitting the ground due to the isGrounded() implementation.
+		//May still occur if player hits a wall before falling to the floor.
+		isJumping = false;
 	}
-
-	void OnCollisionExit(Collision collisionInfo) {
-		canJump = false;
-	}
-
 
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-		float moveHeight;
 
-		if (canJump && Input.GetKeyDown ("space"))
+		if (!isJumping && isGrounded() && Input.GetKeyDown ("space"))
 		{
 			isJumping = true;
-			jumpEnd = Time.time + jump;
+			rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y + jumpHeight, rb.velocity.z); //vertical velocity is current vertical velocity + jump height
+			//rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z); //alternative -- vertical velocity on jump will ALWAYS be the jump height
 		}
-
-		if (isJumping) {
-			//check that the alloted time hasn't passed
-			if (Time.time < jumpEnd) {
-				moveHeight = jumpScale;
-			} else 
-			{
-				moveHeight = jumpScale;
-				isJumping = false;
-			}
-		} else {
-			moveHeight = 0;
-		}
-			
+		
 		if (isPowered) {
 			if (Time.time > powerEnd) {
 				upgrade = 1;
 				isPowered = false;
 			}
 		}
-
 
         //get trig values of the current camera angle
         float angle = Camera.main.transform.eulerAngles.y * Mathf.Deg2Rad;
@@ -81,10 +67,12 @@ public class PlayerController : MonoBehaviour {
         //by adding trig-defined magnitudes
         float adjustedHorizontal = moveHorizontal * trig1 + moveVertical * trig2;
         float adjustedVertical = moveVertical * trig1 - moveHorizontal * trig2;
-
-		Vector3 clampedMovement = Vector3.ClampMagnitude(new Vector3(adjustedHorizontal,moveHeight,adjustedVertical),0.8f);
-		Vector3 movement = new Vector3 (clampedMovement.x, moveHeight, clampedMovement.z);
+        
+        ///Vector3 movement = Vector3.ClampMagnitude(new Vector3(adjustedHorizontal, moveHeight, adjustedVertical), 1.0f);
+		Vector3 movement = new Vector3(adjustedHorizontal,0.0f,adjustedVertical);
 		rb.AddForce(movement * speed * upgrade);
+		Vector2 clampedVelocity = Vector2.ClampMagnitude(new Vector2(rb.velocity.x, rb.velocity.z), speed * upgrade);
+		rb.velocity = new Vector3(clampedVelocity.x, rb.velocity.y, clampedVelocity.y);
 	}
 
 	void OnTriggerEnter(Collider other) 
