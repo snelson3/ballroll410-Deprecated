cb973c2234be910f6d29276221938067fba9a6c4
bryonf <fleming_bryon@hotmail.com>
2016-05-07 23:19:36 -0700

    added improved rotation script, created object moving script (good for platforms)

---
+++ Alpha_Build/Assets/Scripts/ObjectRotateController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotateController : MonoBehaviour {
+	
+	public float speed;
+	public bool horizontalRotate;
+	public bool verticalRotate;
+	
+	private Rigidbody rb;
+	private Vector3 rotate;
+	
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		float hor = horizontalRotate ? 1 : 0;
+		float ver = verticalRotate ? 1 : 0;
+		rotate = Vector3.ClampMagnitude(new Vector3(ver, hor, 0.0f), 1.0f);
+	}
+	
+	void FixedUpdate () {
+		rb.transform.Rotate(rotate * speed * Time.deltaTime);
+	}
+}
---
+++ Alpha_Build/Assets/Scripts/ObjectTranslateController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectTranslateController : MonoBehaviour {
+	
+	public float speed;
+	public float waitTime;
+	public float offset;
+	public bool startReverse;
+	public float end_x;
+	public float end_y;
+	public float end_z;
+	
+	private Rigidbody rb;
+	private Vector3 startPosition, endPosition;
+	private bool reverse, canMove;
+	
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		offset = Mathf.Clamp(offset, 0, 1.0f);
+		startPosition = rb.transform.position;
+		endPosition = new Vector3(end_x, end_y, end_z);
+		rb.transform.position = Vector3.Lerp(startPosition, endPosition, offset);
+		canMove = true;
+		reverse = startReverse;
+	}
+	
+	IEnumerator WaitToMove() {
+		yield return new WaitForSeconds(waitTime);
+		canMove = true;
+	}
+	
+	void FixedUpdate() {
+		if(canMove) {
+			if(!reverse) {
+				rb.transform.position = Vector3.MoveTowards(rb.transform.position, endPosition, Time.deltaTime * speed);
+				if(rb.transform.position == endPosition) {
+					reverse = true;
+					canMove = false;
+					StartCoroutine(WaitToMove());
+				}
+			}
+			else {
+				rb.transform.position = Vector3.MoveTowards(rb.transform.position, startPosition, Time.deltaTime * speed);
+				if(rb.transform.position == startPosition) {
+					reverse = false;
+					canMove = false;
+					StartCoroutine(WaitToMove());
+				}
+			}
+		}
+	}
+}
